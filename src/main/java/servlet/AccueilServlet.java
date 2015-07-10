da103c0510be222b0fcbb2500afdcb270545447e
tristan <tperchec@gmail.com>
2015-07-10 14:30:23 +0200

    Modif connectedPerson

--- src/main/java/servlet/AccueilServlet.java
+++ src/main/java/servlet/AccueilServlet.java
@@ -32,8 +32,7 @@ public class AccueilServlet extends HttpServlet {
 		List<Message> messageList = messageDAO.findAll();
 		List<Message> messagesMonde = new ArrayList<>();
 		List<Message> messagesSuivi = new ArrayList<>();
-		//Person personConnecte = personDAO.findByUsername((String)req.getSession().getAttribute("username"));
-		Person personConnecte = personDAO.findByUsername("faon");
+		Person personConnecte = (Person)req.getSession().getAttribute("connectedPerson");
 
 		List<Person> follows = personConnecte.getFollows();
 		//on charge les messages dans les listes
